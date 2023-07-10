7862401b1461442c812ea8bab73a43c2cfa0c863
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-11 00:26:09 +0300

    Completed test

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let result;
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    result =("This one is on me!")
+  } else if (ride>2000 && ride<=2500) {
+    result=("I will gladly take your thirty bucks.")
+  } else if (ride > 2500) {
+    result="No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+
+
+let city= "NYC";
+function ternaryCheckCity(city){
   // Write your code here!
+  let say= (city==="NYC")? "Ok, sounds good." :"No go.";
+  return say;
 }
 
-function switchOnCharmFromTip(){
+
+
+let respond;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case "generous":
+    respond=("Thank you so much.")
+    break;
+  case "not as generous":
+    respond =("Thank you.")
+    break;
+  default:
+    respond = ("Bye.")
+    break;
+}
+return respond;
 }
